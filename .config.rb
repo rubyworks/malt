ce221e85c18e718193733c8e40d52a1004b390e4
7rans <transfire@gmail.com>
2011-11-14 00:39:03 -0500

    Add simplecov profile for QED. [test]

--- .config.rb
+++ .config.rb
@@ -1,7 +1,12 @@
-
 qed do
   profile :cov do
-
+    require 'simplecov'
+    SimpleCov.start do
+      coverage_dir 'log/coverage'
+      add_group "Formats", "lib/malt/formats"
+      add_group "Engines", "lib/malt/engines"
+    end
   end
 end
+
 
