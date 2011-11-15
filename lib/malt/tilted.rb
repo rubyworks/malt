93b63715a8c06c98f5da573e458d4d8f443588b9
7rans <transfire@gmail.com>
2011-11-14 19:37:01 -0500

    Add support for most Tilt templates.

--- lib/malt/tilted.rb
+++ lib/malt/tilted.rb
@@ -1,21 +1,182 @@
+require 'malt' unless defined?(Malt)
+
 module Malt
+
+  #
+  class Malted
+    #
+    def self.engine_index
+      @@engine_index ||= {}
+    end
+
+    #
+    def self.malt_engine(engine_class=nil)
+      if engine_class
+        @malt_engine = engine_class
+        engine_index[@malt_engine] = self
+      end
+      @malt_engine
+    end
+
+    #
+    def malt_engine
+      self.class.malt_engine
+    end
+
+    #
+    def initialize(file, options={})
+      @file    = file
+      @options = options || {}
+    end
+
+    #
+    def render(*data, &yields)
+      text = File.read(@file)
+      malt_engine.new(@options).render(:text=>text, :file=>@file, :data=>data, &yields)
+    end
+  end
 
   # Tilt compatability layer.
   module Tilted
 
     #
     module Tilt
+      extend Malt::Kernel
 
       #
       def self.new(file, options={})
-        Malt.file(file, options)
+        ext = ext_to_type(File.extname(file))
+        malt_engine = Malt::Engine.defaults[ext] || Malt::Engine.registry[ext].first
+        tilt_engine = Malted.engine_index[malt_engine]
+        tilt_engine.new(file, options)
+      end
+
+
+      class StringTemplate < Malted
+        malt_engine Malt::Engine::String
+      end
+
+
+      # E R U B Y
+
+      #
+      class ERBTemplate < Malted
+        malt_engine Malt::Engine::Erb
       end
 
       #
-      ERBTemplate  = Malt::Format::ERB
+      class ErubisTemplate < Malted
+        malt_engine Malt::Engine::Erubis
+      end
+
+
+      # C U R L E Y  B R A C K E T S
+
+      class LiquidTemplate < Malted
+        malt_engine Malt::Engine::Liquid
+      end
+
+
+      # H T M L
 
       #
-      HAMLTemplate = Malt::Format::HAML
+      class RadiusTemplate < Malted
+        malt_engine Malt::Engine::Radius
+      end
+
+      #
+      class HAMLTemplate < Malted
+        malt_engine Malt::Engine::Haml
+      end
+
+
+      # X M L
+
+      #
+      class BuilderTemplate < Malted
+        malt_engine Malt::Engine::Builder
+      end
+
+      #
+      class MarkabyTemplate < Malted
+        malt_engine Malt::Engine::Markaby
+      end
+
+      #
+      #class NokogiriTemplate < Malted
+      #  malt_engine Malt::Engine::Nokogiri
+      #end
+
+      # R D O C
+
+      #
+      class RDocTemplate < Malted
+        malt_engine Malt::Engine::RDoc
+      end
+
+      # T E X T I L E
+
+      #
+      class RedClothTemplate < Malted
+        malt_engine Malt::Engine::RedCloth
+      end
+
+      # M A R K D O W N
+
+      #
+      class RDiscountTemplate < Malted
+        malt_engine Malt::Engine::RDiscount
+      end
+
+      #
+      class BlueClothTemplate < Malted
+        malt_engine Malt::Engine::BlueCloth
+      end
+
+      #
+      class KramdownTemplate < Malted
+        malt_engine Malt::Engine::Kramdown
+      end
+
+      #class RedcarpetTemplate < Malted
+      #  malt_engine Malt::Engine::Redcarpet  # TODO
+      #end
+
+      #class MarukuTemplate < Malted
+      #  malt_engine Malt::Engine::Maruku  # TODO
+      #end
+
+      # C S S
+
+      #
+      class SassTemplate < Malted
+        malt_engine Malt::Engine::Sass
+      end
+
+      #class ScssTemplate < Malted   # TODO
+      #  malt_engine Malt::Engine::Scss
+      #end
+
+      class LessTemplate < Malted
+        malt_engine Malt::Engine::Less
+      end
+
+
+      class CoffeeScriptTemplate < Malted
+        malt_engine Malt::Engine::Coffee
+      end
+
+      # W I K I
+
+      #
+      #class CreoleTemplate < Malted   # TODO
+      #  malt_engine Malt::Engine::Creole
+      #end
+
+      #
+      #class WikiClothTemplate < Malted   # TODO
+      #  malt_engine Malt::Engine::WikiCloth
+      #end
 
     end
 
@@ -24,5 +185,5 @@ module Malt
 end
 
 # Include in toplevel space.
-include Tilted
+include Malt::Tilted
 
